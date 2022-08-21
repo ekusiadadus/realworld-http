6610ed0e557e4d1f4d7b9a5c87494847f2bf71ec
ekusiadadus <ekusiadadus@gmail.com>
2022-08-22 02:07:29 +0900

    feat(Rust): :fire: Rust Web Server
    https://doc.rust-lang.org/book/ch20-01-single-threaded.html

---
+++ rust/hello-server/src/main.rs
@@ -0,0 +1,26 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main(){
+    let listener = TcpListener::bind("127.0.0.1:18888").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let response = "HTTP/1.1 200 OK¥r¥n¥r¥n";
+    stream.write_all(response.as_bytes()).unwrap();
+}
